d95ed34ddc55e2101b50a2ceba5e9723ca754c7b
samerlagha <samer_la@mail.ru>
2020-03-17 12:22:51 +0200

    commit

--- lesson5/task2/src/profile/printProfile.js
+++ lesson5/task2/src/profile/printProfile.js
@@ -1,4 +1,4 @@
 export default profileData => {
-    const { name, company } = profileData;
-    console.log(`${name} from ${company}`);   
+    const { name, location } = profileData;
+    console.log(`${name} from ${location}`);   
 };
