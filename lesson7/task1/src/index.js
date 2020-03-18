cee2dfcc1420b074cf8f285105e167c793eeec7e
samerlagha <samer_la@mail.ru>
2020-03-18 19:36:04 +0200

    commit

---
+++ lesson7/task1/src/index.js
@@ -0,0 +1,9 @@
+import renderProfile from '../src/profile/renderProfile';
+import './index.css';
+
+const profileData = {
+    name: 'Tom',
+    location: 'The World'
+};
+
+renderProfile(profileData);
