2262c173536a852611bc4d27ce42f028c0b5b1c6
samerlagha <samer_la@mail.ru>
2020-03-19 19:04:49 +0200

    eslint-config-airbnb-base fix

---
+++ lesson9/task1/src/index.js
@@ -0,0 +1,16 @@
+import printProfile from './renderProfile';
+
+const userData = {
+  name: 'Tom',
+  age: 17,
+};
+const profile = {
+  ...userData,
+  company: 'Gromcode',
+};
+printProfile(profile);
+
+const num = 17;
+if (num === 18) {
+  alert('Hi');//eslint-disable-line
+}
