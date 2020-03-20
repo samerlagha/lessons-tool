6d7941f967bc41ed43549e553a76f29c083ffb04
samerlagha <samer_la@mail.ru>
2020-03-20 12:38:34 +0200

    commit

--- lesson9/task2/.eslintrc.js
+++ lesson9/task2/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     extends:'eslint-config-airbnb-base',
     rules:{
         'no-console': 2,
+        'import/prefer-defaul-export':0
     },
     env:{
         browser:true
--- lesson9/todoList/src/list/deleteTask.js
+++ lesson9/todoList/src/list/deleteTask.js
@@ -5,7 +5,6 @@ import { getTasksList, deleteTask } from './tasksGateway';
 function onDeleteTask(event) {
   const deleteBtn = event.target.classList.contains('list-item__delete-btn');
   if (!deleteBtn) return;
-
   const taskId = event.target.parentNode.firstElementChild.dataset.id;
 
   deleteTask(taskId)
