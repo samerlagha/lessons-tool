d79e8dc2fbd7129670046a027f9e91a54ad90e1b
samerlagha <samer_la@mail.ru>
2020-03-21 11:48:55 +0200

    commit

--- lesson9/todoList/.eslintrc.js
+++ lesson9/todoList/.eslintrc.js
@@ -2,13 +2,26 @@ module.export = {
     extends: 'eslint-config-airbnb-base',
     rules: {
         'no-console': 2,
-        'import/prefer-defaul-export':0
+        'import/prefer-defaul-export':0,
+        "prettier/prettier": "error"
+       
     },
     env: {
-        browser: true,
+         browser: true,
+        "es6": true,
+     
     },
+   
     parserOptions: {
         ecmaVersion: 10,
-        sourceType: 'module'
-    }
+   
+        sourceType: 'module',
+
+        "ecmaFeatures": {
+            "jsx": true,
+            "modules": true
+        }
+    },
+    "plugins": ["prettier"],
+  "extends": ["prettier"],
 }
